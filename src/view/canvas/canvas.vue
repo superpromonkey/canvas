891b3d763067135a1f173d159f07edc2faafc719
SPK <gan4248344532@163.com>
2019-07-04 16:38:23 +0800

    创建canvas

---
+++ src/view/canvas/canvas.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="canvasbox">
+        <canvas ref="gcanvas"></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'gcanvas',
+    data(){
+        return{
+
+        }
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
+
+
