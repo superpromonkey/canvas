f2b0c5b9c13540bb763fe918f1658fa4f5adcc1b
SPK <gan4248344532@163.com>
2019-07-04 17:05:15 +0800

    配置路由

---
+++ src/view/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="index">
+        sdjfnsj,nd
+        <canvas ref="gcanvas"></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'index',
+    data(){
+        return{
+
+        }
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
+
+
